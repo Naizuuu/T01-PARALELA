5166db311b86fd4a6a4febdd38e64231b6eb2418
Sebastian <sebastian.munozo@utem.cl>
2021-05-10 01:06:11 -0400

    Se eliminan comillas y se transforma de string a float los promedios

--- funciones.cpp
+++ funciones.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <fstream>
 #include <string>
+#include <algorithm>
 
 #define NOTAS "archivos/estudiantes.csv"
 
@@ -13,9 +14,11 @@ void leerdatos() {
     string linea;
     char delimitador = ';';
 
-    while(getline(archivo, linea)) {
-        stringstream stream(linea);  //convertir cadena a stream
+    while(getline(archivo, linea)) { // Entran así -> "ID";"NOMBRE";"PROM_LENG"; []...
+        stringstream stream(linea);  // Convertir cadena a stream
         string idNum, idUnico, lenguaje, ingles, mats, ciencias, historia, tecnologia, arte, edfisica;
+
+        // Obtener los datos del .csv
         getline(stream, idNum, delimitador);
         getline(stream, idUnico, delimitador);
         getline(stream, lenguaje, delimitador);
@@ -23,9 +26,22 @@ void leerdatos() {
         getline(stream, mats, delimitador);
         getline(stream, ciencias, delimitador);
         getline(stream, historia, delimitador);
-        getline(stream, tecnologia, delimitador);
-        getline(stream, arte, delimitador);       
+        getline(stream, tecnologia, delimitador); 
+        getline(stream, arte, delimitador);
         getline(stream, edfisica, delimitador);
+
+        // Eliminar las comillas
+        idNum.erase(remove(idNum.begin(), idNum.end(), '\"'), idNum.end()); 
+        idUnico.erase(remove(idUnico.begin(), idUnico.end(), '\"'), idUnico.end());
+        lenguaje.erase(remove(lenguaje.begin(), lenguaje.end(), '\"'), lenguaje.end());
+        ingles.erase(remove(ingles.begin(), ingles.end(), '\"'), ingles.end());
+        mats.erase(remove(mats.begin(), mats.end(), '\"'), mats.end());
+        ciencias.erase(remove(ciencias.begin(), ciencias.end(), '\"'), ciencias.end());
+        historia.erase(remove(historia.begin(), historia.end(), '\"'), historia.end());
+        tecnologia.erase(remove(tecnologia.begin(), tecnologia.end(), '\"'), tecnologia.end());
+        arte.erase(remove(arte.begin(), arte.end(), '\"'), arte.end());   
+        edfisica.erase(remove(edfisica.begin(), edfisica.end(), '\"'), edfisica.end());
+
         // Imprimir
         cout << "==================" << endl;
         cout << "ID: " << idNum << endl;
@@ -38,6 +54,13 @@ void leerdatos() {
         cout << "Promedio Tecnologia: " << tecnologia << endl;
         cout << "Promedio Arte: " << arte << endl;
         cout << "Promedio Edfisica: " << edfisica << endl;
+        
+        // Calculo de promedios
+        float leng, mate;
+        if(istringstream(lenguaje) >> leng && istringstream(mats) >> mate){
+            cout << endl << "Promedio entre Lenguaje y Matematicas: " << (leng + mate) / 2 << endl;
+        }
+        
     }
     archivo.close();
 }
